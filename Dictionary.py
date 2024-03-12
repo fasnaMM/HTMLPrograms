b3ff9b59070304f5ae2184fb11f3c54db63097bb
FASNA M M <154966116+fasnaMM@users.noreply.github.com>
2024-03-12 06:21:06 +0530

    Add files via upload

---
+++ Dictionary.py
@@ -0,0 +1,34 @@
+#create dictionary
+
+student={"name":"Fasna",
+         "admission_num":1292,
+         "Department":"MCA"}
+print(student)
+
+#type
+student={"name":"Fasna",
+         "admission_num":1292,
+         "Department":"MCA"}
+print(type(student))
+
+
+#length
+student={"name":"Fasna",
+         "admission_num":1292,
+         "Department":"MCA"}
+print(len(student))
+
+#accessing
+student={"name":"Fasna",
+         "admission_num":1292,
+         "Department":"MCA"}
+print(student["name"])
+
+#copy
+student={"name":"Fasna",
+         "admission_num":1292,
+         "Department":"MCA"}
+student2=student.copy()
+print(student2)
+
+
