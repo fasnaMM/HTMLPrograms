bc91a9ea880aeb3be4f1509ac1f9b067432c505f
FASNA M M <154966116+fasnaMM@users.noreply.github.com>
2024-03-12 06:24:55 +0530

    Add files via upload

---
+++ datetime.py
@@ -0,0 +1,194 @@
+#weekday short
+import datetime
+x=datetime.datetime(2024,2,22)
+print(x.strftime("%a"))
+
+#weekday full
+import datetime
+x=datetime.datetime(2024,2,22)
+print(x.strftime("%A"))
+
+#weekday number
+import datetime
+x=datetime.datetime(2024,2,22)
+print(x.strftime("%w"))
+
+#day of month
+import datetime
+x=datetime.datetime(2024,2,22)
+print(x.strftime("%d"))
+
+# month of name short
+import datetime
+x=datetime.datetime(2024,2,22)
+print(x.strftime("%b"))
+
+# month of name full
+import datetime
+x=datetime.datetime(2024,2,22)
+print(x.strftime("%B"))
+
+# month as a number
+import datetime
+x=datetime.datetime(2024,2,22)
+print(x.strftime("%m"))
+
+#year short
+import datetime
+x=datetime.datetime(2024,2,22)
+print(x.strftime("%y"))
+
+#year full
+import datetime
+x=datetime.datetime(2024,2,22)
+print(x.strftime("%Y"))
+
+#hour full
+import datetime
+x=datetime.datetime(2024,2,22)
+print(x.strftime("%H"))
+
+#hour 
+import datetime
+x=datetime.datetime(2024,2,22)
+print(x.strftime("%I"))
+
+#am/pm
+import datetime
+x=datetime.datetime(2024,2,22)
+print(x.strftime("%p"))
+
+
+#minute
+import datetime
+x=datetime.datetime(2024,2,22)
+print(x.strftime("%m"))
+
+#second
+import datetime
+x=datetime.datetime(2024,2,22)
+print(x.strftime("%S"))
+
+#microsecond
+import datetime
+x=datetime.datetime(2024,2,22)
+print(x.strftime("%f"))
+
+#UTS
+import datetime
+x=datetime.datetime(2024,2,22)
+print(x.strftime("%z"))
+
+#timezone
+import datetime
+x=datetime.datetime(2024,2,22)
+print(x.strftime("%Z"))
+
+#day number
+import datetime
+x=datetime.datetime(2024,2,22)
+print(x.strftime("%j"))
+
+#week number
+import datetime
+x=datetime.datetime(2024,2,22)
+print(x.strftime("%U"))
+
+#week number
+import datetime
+x=datetime.datetime(2024,2,22)
+print(x.strftime("%W"))
+
+#local version
+import datetime
+x=datetime.datetime(2024,2,22)
+print(x.strftime("%c"))
+
+#century
+import datetime
+x=datetime.datetime(2024,2,22)
+print(x.strftime("%C"))
+
+#local version date
+import datetime
+x=datetime.datetime(2024,2,22)
+print(x.strftime("%x"))
+
+#local version time
+import datetime
+x=datetime.datetime(2024,2,22)
+print(x.strftime("%X"))
+
+#A%
+import datetime
+x=datetime.datetime(2024,2,22)
+print(x.strftime("%%"))
+
+#ISO year
+import datetime
+x=datetime.datetime(2024,2,22)
+print(x.strftime("%G"))
+
+#ISO weakday
+import datetime
+x=datetime.datetime(2024,2,22)
+print(x.strftime("%u"))
+
+#ISO weaknumber
+import datetime
+x=datetime.datetime(2024,2,22)
+print(x.strftime("%V"))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
